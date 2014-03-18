fc6e597e2805a9b50622a01c81e99afa5b05598c
Greyson Parrelli <greyson.parrelli@gmail.com>
2014-03-18 03:32:43 -0400

    Cleaned up character math a little.

--- CharacterMathDemo.java
+++ CharacterMathDemo.java
@@ -5,33 +5,26 @@
  * Format", and it means that Java characters are represented by 16-bit
  * strings. 
  */
-
 public class CharacterMathDemo {
     
     public static void main(String[] args) {
-        // Something simple
-        /*
-         * Characters in Java are generally enclosed with apostrophes ('').
-         * Java can use the 16-bit string of a character to an integer equivalent.
-         */
+        // Characters in Java are generally enclosed with apostrophes ('').
+        // Java can convert the 16-bit string of a character to an integer equivalent.
         int five = '5';
         System.out.println("The character '5' has a decimal value of " + five);
 
-        /*
-         * Because characters have decimal equivalents, we can use the difference
-         * between two characters. Here we demonstrate how to find the position of
-         * a letter in the alphabet.
-         */
+        
+        // Because characters have decimal equivalents, we can use the difference
+        // between two characters. Here we demonstrate how to find the position of
+        // a letter in the alphabet.
         int positionOfG = 'g' - 'a';
         System.out.println("The character 'g' is the " + (positionOfG + 1) + "th letter of the alphabet.");
 
-        /*
-         * Conversion can go both ways if we type cast an integer as a character.
-         * How can you create a your own ascii table using this?
-         */
+        // Conversion can go both ways if we type cast an integer as a character.
+        // How can you create a your own ascii table using this?
         System.out.println("65 is the decimal value of " + (char)(65));
 
-        //Our own "toUpper" function
+        // Our own "toUpper" function
         System.out.println("The uppercase of 'a' is " + toUpper('a'));
 
         // Something a bit trickier
@@ -40,19 +33,40 @@ public class CharacterMathDemo {
         System.out.println(n1 + n2);
     }
 
+    /**
+     * Converts a given lowercase letter to an uppercase letter
+     * @param  c The lowercase number you want converted
+     * @return   If c was a lowercase letter, it returns the uppercase version. Otherwise,
+     *           it just returns c.
+     */
     private static char toUpper(char c) {
+        // Check for valid input
         if(c >= 'a' && c <= 'z') {
-            c += 'A' - 'a'; //Add the difference between the uppercase and lowercase alphabet
+            //Add the difference between the uppercase and lowercase alphabet
+            c += 'A' - 'a'; 
         }
         return c;
     }
+
+    /**
+     * Converts a string to a base 10 integer
+     * @param  s The string you want coverted to a base 10 integer
+     * @return   A base 10 integer represeting the given string
+     */
     private static int parseInt(String s) {
         int sum = 0;
         int placeValue = 1;
+
+        // Go through the string from right to left
         for (int i = s.length() - 1; i >= 0; i--) {
-            int val = s.charAt(i) - '0'; //extract the decimal value of each character
-            sum += val * placeValue; //and add to the sum at the current place value
-            placeValue *= 10; //increase the place value
+            // Extract the decimal value of each character
+            int val = s.charAt(i) - '0'; 
+
+            // Add to the sum at the current place value
+            sum += val * placeValue;
+
+            // Increase the place value
+            placeValue *= 10;
         }
         return sum;
     }
